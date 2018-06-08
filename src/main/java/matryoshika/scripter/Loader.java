6a0bfc63611a109eb73c633fc749e8b66ef6d525
Unknown <mad.matryoshika@gmail.com>
2018-06-08 10:25:27 +0200

    Improved recursive script scan

--- src/main/java/matryoshika/scripter/Loader.java
+++ src/main/java/matryoshika/scripter/Loader.java
@@ -2,16 +2,16 @@ package matryoshika.scripter;
 
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileFilter;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
 
-import net.minecraftforge.fml.common.eventhandler.Event;
+import org.apache.commons.io.FileUtils;
 
 public class Loader {
 
@@ -20,30 +20,30 @@ public class Loader {
 	public static Map<String, String> scriptNames = new HashMap<String, String>();
 
 	public static void loadScripts(File directory) {
-		
-		Scripter.logger.error("Running loadScripts");
-		if(!directory.exists())
-			directory.mkdirs();
-		
-		scripts = new ArrayList<File>();
-		File[] fArray = directory.listFiles();
 
-		for (File file : fArray) {
+		Scripter.logger.error("Running loadScripts");
+		if (!directory.exists())
+			directory.mkdirs();
+
+		scripts = new ArrayList<File>();
+		Collection<File> ffiles = FileUtils.listFiles(directory, new String[] { "js" }, true);
+
+		for (File file : ffiles) {
 			if (file.isFile())
 				scripts.add(file);
 		}
-		
+
 		mapScripts();
 	}
 
 	public static void mapScripts() {
 		Scripter.logger.error("Running mapScripts");
 		for (File file : scripts) {
-			if(!file.getName().endsWith("js") || !file.getName().contains("scripter")) {
-				Scripter.logger.error("Skipping " + file.getName());
+			if (!file.getName().endsWith("js") || !file.getName().contains("scripter")) {
+				Scripter.logger.error("Skipping over " + file.getName());
 				continue;
 			}
-			
+
 			String txt = "";
 			String script = "";
 			try {
@@ -57,15 +57,15 @@ public class Loader {
 				e.printStackTrace();
 			}
 			txt = txt.replace("//", "");
-			
-			if(!mappedScripts.containsKey(txt))
+
+			if (!mappedScripts.containsKey(txt))
 				mappedScripts.put(txt, new ArrayList<String>());
-			
+
 			mappedScripts.get(txt).add(script);
 			scriptNames.put(script, file.getName());
 		}
 	}
-	
+
 	public static String getScriptName(String script) {
 		return scriptNames.get(script);
 	}
